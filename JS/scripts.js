d1338f4af14e965a13bfecba2af3652d4ada5a1c
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-12 14:28:52 -0700

    added the word answer in the javascript result field. Updated styling in css for the output id.
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    $("#output").text(result);
+    $("#output").text("Answer: " + result);
   });
 // Subtract
   $("form#subtract").submit(function(event) {
@@ -33,7 +33,7 @@ $(document).ready(function() {
      const number1 = parseInt($("#subtract1").val());
      const number2 = parseInt($("#subtract2").val());
      const result = subtract(number1, number2);
-     $("#output").text(result);
+     $("#output").text("Answer: " + result);
    });
 // Multiply
    $("form#multiply").submit(function(event) {
@@ -41,7 +41,7 @@ $(document).ready(function() {
      const number1 = parseInt($("#multiply1").val());
      const number2 = parseInt($("#multiply2").val());
      const result = multiply(number1, number2);
-     $("#output").text(result);
+     $("#output").text("Answer: " + result);
    });
   // Divide
   $("form#divide").submit(function(event) {
@@ -49,6 +49,6 @@ $(document).ready(function() {
      const number1 = parseInt($("#divide1").val());
      const number2 = parseInt($("#divide2").val());
      const result = divide(number1, number2);
-     $("#output").text(result);
+     $("#output").text("Answer: " + result);
    });
 });
