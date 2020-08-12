aa8e89844c7e3f0b0ecb1d76f119dcb92337c125
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-12 13:53:45 -0700

    updated calculator project from lession, added css styling for form-control, added form id subtract, added subtract, multiply, divide in the javascript user interface logic
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,10 +1,54 @@
+// Business logic:
+
+function add(number1, number2) {
+  return number1 + number2;
+}
 
 function subtract(number1, number2) {
   return number1 - number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter a number:"));
+function multiply(number1, number2) {
+  return number1 * number2;
+}
 
-alert(subtract(number1, number2));
-alert(result);
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+// User interface logic:
+
+$(document).ready(function() {
+// Add
+  $("form#add").submit(function(event) {
+   event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+// Subtract
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+     const number1 = parseInt($("#subtract1").val());
+     const number2 = parseInt($("#subtract2").val());
+     const result = subtract(number1, number2);
+     $("#output").text(result);
+   });
+// Multiply
+   $("form#subtract").submit(function(event) {
+    event.preventDefault();
+     const number1 = parseInt($("#subtract1").val());
+     const number2 = parseInt($("#subtract2").val());
+     const result = subtract(number1, number2);
+     $("#output").text(result);
+   });
+  // Divide
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+     const number1 = parseInt($("#subtract1").val());
+     const number2 = parseInt($("#subtract2").val());
+     const result = subtract(number1, number2);
+     $("#output").text(result);
+   });
+});
