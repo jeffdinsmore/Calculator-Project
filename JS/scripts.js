491ebce7407111b13f90e3edadb216e5e50a8cc7
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-12 14:15:53 -0700

    updated javascript user interface logic for multiply and divide; added user interface elements in html for multiply and divide and gave them some css styling from bootstrap. Also added a header height to bring the container down a bit
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -36,19 +36,19 @@ $(document).ready(function() {
      $("#output").text(result);
    });
 // Multiply
-   $("form#subtract").submit(function(event) {
+   $("form#multiply").submit(function(event) {
     event.preventDefault();
-     const number1 = parseInt($("#subtract1").val());
-     const number2 = parseInt($("#subtract2").val());
-     const result = subtract(number1, number2);
+     const number1 = parseInt($("#multiply1").val());
+     const number2 = parseInt($("#multiply2").val());
+     const result = multiply(number1, number2);
      $("#output").text(result);
    });
   // Divide
-  $("form#subtract").submit(function(event) {
+  $("form#divide").submit(function(event) {
     event.preventDefault();
-     const number1 = parseInt($("#subtract1").val());
-     const number2 = parseInt($("#subtract2").val());
-     const result = subtract(number1, number2);
+     const number1 = parseInt($("#divide1").val());
+     const number2 = parseInt($("#divide2").val());
+     const result = divide(number1, number2);
      $("#output").text(result);
    });
 });
